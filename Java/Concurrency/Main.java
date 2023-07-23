3be22540e4fa3686d42ae30426c951818cb0b809
arshad404 <khan36390@gmail.com>
2023-07-23 18:31:29 +0530

    Remove unnecessary files

--- Java/Concurrency/Main.java
+++ Java/Concurrency/Main.java
@@ -4,34 +4,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Solution s = new Solution();
-        System.out.println(s.letterCombinations("23"));
+        System.out.println("Hello World");
 
-    }
-}
-
-class Solution {
-    List<String> store = new ArrayList<>(Arrays.asList("", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"));
-
-    public List<String> letterCombinations(String digits) {
-         StringBuilder sb = new StringBuilder();
-         List<String> res = new ArrayList<>();
-
-         dfs(0, digits, res, sb);
-         return res;
-    }
-
-    public void dfs(int index, String digits, List<String> res, StringBuilder sb) {
-        if(index >= digits.length()) {
-            res.add(sb.toString());
-            return;
-        }
-
-        String options = store.get(Character.getNumericValue(digits.charAt(index)));
-        for(char option : options.toCharArray()) {
-            sb.append(option);
-            dfs(index+1, digits, res, sb);
-            sb.deleteCharAt(sb.length() - 1);
-        }
     }
 }
